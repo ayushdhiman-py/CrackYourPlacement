9d6b7a1f3c7bbc9a2a8d94e800c4fa274093dc2a
aayushdhimann <ayushdhiman0107@gmail.com>
2024-07-21 11:41:52 +0530

    Find the Duplicate Number

---
+++ DuplicateNumber.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        vector<int> check(nums.size(), 0);
+        for(int i = 0; i < nums.size(); i++) {
+            if(check[nums[i]] > 0) {
+                return nums[i];
+            }
+            check[nums[i]]++;
+        }
+        return 0;
+    }
+};
