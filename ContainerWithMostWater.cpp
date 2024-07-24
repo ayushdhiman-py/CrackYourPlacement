47eab6e66793e582ed9154145edb3a7a7a4b32aa
aayushdhimann <ayushdhiman0107@gmail.com>
2024-07-24 22:49:00 +0530

    17. Container with most water| DONE

---
+++ ContainerWithMostWater.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int ans=0;
+        int i=0,j=height.size()-1;
+
+        while(i<j){
+            int area=(j-i)*min(height[i],height[j]);
+            ans=max(ans,area);
+            if(height[i]<height[j]){
+                i++;
+            }
+            // else(height[i]>height[j]){
+            //     j--;
+            // }
+            else{
+                j--;
+            }
+        }
+        return ans;
+    }
+};
